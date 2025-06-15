63d8cb982107ea2f8b0eba729ef9232288b958d9
quanghv20 <quanghv20@gmail.com>
2025-06-15 09:43:27 +0700

    update edit profile modal

--- src/components/user-profile/UserMetaCard.tsx
+++ src/components/user-profile/UserMetaCard.tsx
@@ -10,7 +10,8 @@ import { IProfile } from '@/type/profile.type';
 import { FacebookIcon, GithubIcon, InstagramIcon, LinkedinIcon } from '@/icons';
 import { ISocial } from '@/type/profile.type';
 import AvatarText from '../ui/avatar/AvatarText';
-import EditMetaProfileModal from './EditMetaProfileModal';
+import { JSLifeTable } from '../tables/JSLifeTable';
+import BasicTableOne from '../tables/BasicTableOne';
 
 type PropsType = {
   profile: IProfile;
@@ -25,6 +26,9 @@ const socialIcons: Record<string, JSX.Element> = {
 
 export default function UserMetaCard({ profile }: PropsType) {
   const { isOpen, openModal, closeModal } = useModal();
+
+  const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+  const [fileSelected, setFileSelected] = useState(false);
 
   return (
     <>
@@ -92,11 +96,87 @@ export default function UserMetaCard({ profile }: PropsType) {
         </div>
       </div>
 
-      <EditMetaProfileModal
-        isOpen={isOpen}
-        onClose={closeModal}
-        profile={profile}
-      />
+      <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[1000px] m-4">
+        <div className="no-scrollbar relative w-full max-w-[1000px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
+          <div className="px-2 pr-14">
+            <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
+              Edit Profile
+            </h4>
+            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
+              Update your details to keep your profile up-to-date.
+            </p>
+            <div>
+              <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                Profile Picture
+              </h5>
+              <div className="flex items-center gap-6">
+                <div className="h-20 w-20 rounded-full bg-gray-200 dark:bg-gray-700 overflow-hidden border border-gray-300 dark:border-gray-600">
+                  <img
+                    src={avatarPreview || profile.avatarUrl || ''}
+                    alt="Avatar Preview"
+                    className="h-full w-full object-cover"
+                  />
+                </div>
+
+                <div>
+                  <Label htmlFor="avatarUpload" className="mb-1 block">
+                    Upload new avatar
+                  </Label>
+
+                  <div className="flex items-center gap-3">
+                    <label
+                      htmlFor="avatarUpload"
+                      className="cursor-pointer rounded-md bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700"
+                    >
+                      Choose File
+                    </label>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      {fileSelected ? 'File selected' : 'No file chosen'}
+                    </span>
+                  </div>
+
+                  <input
+                    id="avatarUpload"
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    onChange={(e) => {
+                      const file = e.target.files?.[0];
+                      if (file) {
+                        const url = URL.createObjectURL(file);
+                        setAvatarPreview(url);
+                        setFileSelected(true);
+                      } else {
+                        setFileSelected(false);
+                      }
+                    }}
+                  />
+                  <p className="mt-1 text-xs text-gray-500 dark:text-gray-400">Max size: 2MB</p>
+                </div>
+              </div>
+            </div>
+            <div className="mt-7">
+              <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
+                Social Links
+              </h5>
+            </div>
+          </div>
+          <form className="flex flex-col">
+            <div className="custom-scrollbar h-[450px] overflow-y-auto px-2 pb-3">
+              <BasicTableOne />
+            </div>
+            <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
+              <Button size="sm" variant="outline" onClick={closeModal}>
+                Close
+              </Button>
+              <Button size="sm" onClick={() => { }}>
+                Save Changes
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+
 
     </>
   );
--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -7,10 +7,88 @@ function wait(ms: number) {
 
 export const profileService = {
   async getProfile(username: string): Promise<IProfile> {
-    const res = await requestApi<IProfile>({
-      method: 'GET',
-      url: `/profile/username/${username}`,
-    });
+    // const res = await requestApi<IProfile>({
+    //   method: 'GET',
+    //   url: `/profile/username/${username}`,
+    // });
+
+    const res = {
+      "data": {
+        "_id": "6846fff12a8878b6b959ba2c",
+        "username": "quanghv",
+        "alias": "Quang Cối",
+        "firstName": "Hà Văn",
+        "lastName": "Quang",
+        "email": "quanghv20@gmail.com",
+        "phone": "0986915765",
+        "address": "Hanoi, Vietnam",
+        "avatarUrl": "https://example.com/avatar.jpg",
+        "bio": "Fullstack Developer",
+        "stack": "NodeJS & ReactJS",
+        "education": {
+          "logo": "https://example.com/university-logo.png",
+          "name": "FPT University",
+          "major": "Software Engineering",
+          "fromDate": "09/2018",
+          "toDate": "09/2022",
+          "degree": "Bachelor",
+          "gpa": "3.1",
+          "description": "",
+          "_id": "684700502a8878b6b959ba5f"
+        },
+        "companies": [
+          {
+            "logo": "https://example.com/company-a-logo.png",
+            "name": "FPT Software.",
+            "fromDate": "02/2023",
+            "toDate": "03/2024",
+            "position": "NodeJS & ReactJS Developer",
+            "description": "",
+            "_id": "684700502a8878b6b959ba5d"
+          },
+          {
+            "logo": "https://example.com/company-b-logo.png",
+            "name": "Viettel",
+            "fromDate": "04/2024",
+            "toDate": "Present",
+            "position": "ReactJS Developer",
+            "description": "",
+            "_id": "684700502a8878b6b959ba5e"
+          }
+        ],
+        "socials": [
+          {
+            "icon": "github",
+            "name": "Github",
+            "link": "https://github.com/quanghv20",
+            "_id": "684700502a8878b6b959ba59"
+          },
+          {
+            "icon": "linkedin",
+            "name": "Linkedin",
+            "link": "https://www.linkedin.com/in/quanghv20",
+            "_id": "684700502a8878b6b959ba5a"
+          },
+          {
+            "icon": "facebook",
+            "name": "Facebook",
+            "link": "https://www.facebook.com/quanghv20",
+            "_id": "684700502a8878b6b959ba5b"
+          },
+          {
+            "icon": "instagram",
+            "name": "Instagram",
+            "link": "https://www.instagram.com/quanghv20",
+            "_id": "684700502a8878b6b959ba5c"
+          }
+        ],
+        "__v": 0
+      },
+      "message": "Retrieved profile by username successfully",
+      "statusCode": 200,
+      "path": "/my-cms/api/profile/username/quanghv",
+      "timestamp": "2025-06-15T01:49:08.871Z"
+    }
 
     return res.data;
   }
